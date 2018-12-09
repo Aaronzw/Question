254ae3ad5c92430ddcc502a88ade5240406747b0
ZhuangWei <905280097@qq.com>
2018-12-09 21:08:28 +0800

    前端header和footer；配置

--- src/main/java/com/zhuangwei/fzu/Question/controller/IndexController.java
+++ src/main/java/com/zhuangwei/fzu/Question/controller/IndexController.java
@@ -1,5 +1,17 @@
 package com.zhuangwei.fzu.Question.controller;
 
-public class IndexController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
+public class IndexController {
+    @RequestMapping(path = {"/test"}, method = {RequestMethod.GET, RequestMethod.POST})
+    public String index(Model model) {
+        int a=2;
+        int b=2*a;
+        return "test";
+    }
 }
