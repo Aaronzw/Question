55866a646461ac716160015d0c8e0aaaa9a6de0c
ZhuangWei <905280097@qq.com>
2018-12-26 23:52:26 +0800

    11

--- src/main/java/com/wenda/controller/IndexController.java
+++ src/main/java/com/wenda/controller/IndexController.java
@@ -4,13 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
-    @RequestMapping(path = {"/index","/"}, method = {RequestMethod.GET, RequestMethod.POST})
-    public String index(Model model) {
-        return "index";
-    }
+//    @RequestMapping(path = {"/index","/"}, method = {RequestMethod.GET, RequestMethod.POST})
+//    public String index(Model model,@RequestParam(value = "pop",defaultValue = "0")int pop) {
+//        model.addAttribute("vos",);
+//        return "index";
+//    }
     @RequestMapping(path = {"/test"}, method = {RequestMethod.GET, RequestMethod.POST})
     public String test(Model model) {
         return "test";
--- src/main/java/com/wenda/service/QuestionService.java
+++ src/main/java/com/wenda/service/QuestionService.java
@@ -1,4 +1,38 @@
 package com.wenda.service;
 
+import com.wenda.dao.QuestionDao;
+import com.wenda.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
+
+import java.util.List;
+
+@Service
 public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    @Autowired
+    SensitiveService sensitiveService;
+
+    public Question getById(int id){
+        return questionDao.getById(id);
+    }
+    public int addQuestion(Question question){
+        question.setContent(HtmlUtils.htmlEscape(question.getContent()));
+        question.setTitle(HtmlUtils.htmlEscape(question.getTitle()));
+
+        question.setTitle(sensitiveService.filter(question.getTitle()));
+        question.setContent(sensitiveService.filter(question.getContent()));
+        return questionDao.addQuestion(question)>0?question.getId():0;
+    }
+
+    public List<Question> getLatestQuestions(int userId,int offset,int limit){
+        return questionDao.selectLatestQuestions(userId,offset,limit);
+    }
+    public int updateCommentCount(int id,int count){
+        return questionDao.updateCommentCount(id,count);
+    }
 }
