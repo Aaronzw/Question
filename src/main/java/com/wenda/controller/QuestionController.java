892db4a51e1fd61a62dda54fbcee2ee5d0420347
zhuangw <905380097@qq.com>
2018-12-28 17:05:23 +0800

    问题和评论，以及json

--- src/main/java/com/wenda/controller/QuestionController.java
+++ src/main/java/com/wenda/controller/QuestionController.java
@@ -1,9 +1,21 @@
 package com.wenda.controller;
 
+import com.wenda.model.HostHolder;
+import com.wenda.service.CommentService;
+import com.wenda.service.QuestionService;
+import com.wenda.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class QuestionController {
-
+    @Autowired
+    HostHolder hostHolder;
+    @Autowired
+    QuestionService questionService;
+    @Autowired
+    UserService userService;
+    @Autowired
+    CommentService commentService;
 
 }
--- src/main/java/com/wenda/util/WendaUtil.java
+++ src/main/java/com/wenda/util/WendaUtil.java
@@ -1,9 +1,11 @@
 package com.wenda.util;
 
+import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.security.MessageDigest;
+import java.util.Map;
 
 public class WendaUtil {
     private static final Logger logger = LoggerFactory.getLogger(WendaUtil.class);
@@ -35,5 +37,25 @@ public class WendaUtil {
             logger.error("生成MD5失败", e);
             return null;
         }
+    }
+
+    public static String getJSONString(int code){
+        JSONObject json=new JSONObject();
+        json.put("code",code);
+        return json.toString();
+    }
+    public static String getJSONString(int code,String msg){
+        JSONObject json=new JSONObject();
+        json.put("code",code);
+        json.put("msg",msg);
+        return json.toString();
+    }
+    public static String getJSONString(int code, Map<String,Object> map){
+        JSONObject json=new JSONObject();
+        json.put("code",code);
+        for(Map.Entry<String,Object> entry:map.entrySet()){
+            json.put(entry.getKey(),entry.getValue());
+        }
+        return json.toString();
     }
 }
