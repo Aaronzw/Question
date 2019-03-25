6d1812ecef9021fbe49f9e9a986b99509c0ebd24
ZhuangWei <905280097@qq.com>
2019-03-25 16:37:58 +0800

    文件操作

--- src/main/java/com/wenda/controller/FileController.java
+++ src/main/java/com/wenda/controller/FileController.java
@@ -2,7 +2,9 @@ package com.wenda.controller;
 
 import com.wenda.model.Comment;
 import com.wenda.model.EntityType;
+import com.wenda.service.FileService;
 import com.wenda.util.WendaUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,14 +13,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
 
 @Controller
 public class FileController {
+    @Autowired
+    FileService fileService;
     @RequestMapping(path ={"/upload/"},method = {RequestMethod.POST})
-    @ResponseBody
-    public String likeComment(@RequestParam("file")MultipartFile multipartFile){
-        int a=1+1;
-        return "hi";
+    /*multipartFile转file，再上传到腾讯云，再删除file，以免占空间*/
+    public String likeComment(@RequestParam("file")MultipartFile multipartFile) throws Exception{
+        File toFile=null;
+        if(multipartFile.getSize()<=0||multipartFile.equals("")){
+            return null;
+        }else {
+            InputStream ins=null;
+            ins=multipartFile.getInputStream();
+            toFile = new File(multipartFile.getOriginalFilename());
+            fileService.inputStreamToFile(ins,toFile);
+            toFile.delete();
+            ins.close();
+        }
+        return "redirect:/test";
     }
 
 }
--- src/main/java/com/wenda/service/FileService.java
+++ src/main/java/com/wenda/service/FileService.java
@@ -1,7 +1,55 @@
 package com.wenda.service;
 
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.ClientConfig;
+import com.qcloud.cos.auth.BasicSessionCredentials;
+import com.qcloud.cos.model.PutObjectRequest;
+import com.qcloud.cos.model.PutObjectResult;
+import com.qcloud.cos.region.Region;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 @Service
-public class FileService {
+public class FileService implements InitializingBean {
+    public void inputStreamToFile(InputStream ins, File file) {
+        try {
+            OutputStream os = new FileOutputStream(file);
+            int bytesRead = 0;
+            byte[] buffer = new byte[8192];
+            while ((bytesRead = ins.read(buffer, 0, 8192)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            os.close();
+            ins.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void uploadToTencentCos(File file,String new_filename){
+        File localFile = new File("src/test/resources/len5M.txt");
+// 指定要上传到的存储桶
+        String bucketName = "demoBucket-1250000000";
+// 指定要上传到 COS 上对象键
+        String key = "upload_single_demo.txt";
+
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, localFile);
+        PutObjectResult putObjectResult = cosClient.putObject(putObjectRequest);
+    }
+    public void afterPropertiesSet() throws Exception {
+        // 1 传入获取到的临时密钥 (tmpSecretId, tmpSecretKey, sessionToken)
+        BasicSessionCredentials cred = new BasicSessionCredentials("a-demo-secretId", "a-demo-secretKey", "a-demo-session-token");
+// 2 设置 bucket 的区域, COS 地域的简称请参照 https://cloud.tencent.com/document/product/436/6224
+// clientConfig 中包含了设置 region, https(默认http), 超时, 代理等 set 方法, 使用可参见源码或者接口文档 FAQ
+        ClientConfig clientConfig = new ClientConfig(new Region("ap-beijing-1"));
+// 3 生成 cos 客户端
+        COSClient cosClient = new COSClient(cred, clientConfig);
+// bucket 的命名规则为{name}-{appid} ，此处填写的存储桶名称必须为此格式
+        String bucketName = "mybucket-1251668577";
+
+    }
 }
